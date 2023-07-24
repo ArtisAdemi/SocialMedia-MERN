14e5f273aea66986ac960a5912a64fa1f6e77c85
ArtisAdemi <artis.ademi@gmail.com>
2023-07-24 18:06:51 +0200

    Authorization

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import { register } from "./controllers/auth.json"
+// require('dotenv').config();
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -36,6 +37,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+
+//ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
